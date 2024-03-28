6777641302759745b8dee6a976766a4da7ab9d82
Ous19998 <filalioussama839@gmail.com>
2024-03-27 19:07:13 -0700

    easy

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,11 +4,13 @@
 file { '/etc/ssh/ssh_config':
   ensure ==> present,
 }
+
 file_line { 'turn off passwd auth':
  path  => '/etc/ssh/ssh_config',
  line  => 'PasswordAuthentication no',
  match => '^#PasswordAuthentication',
 }
+
 file_line { 'Declare identity file':
  path  => '/etc/ssh/ssh_config',
  line  => 'IdentityFile ~/.ssh/school',
